65cbe088d15cb5e6d9c0431bb283f3a729036074
ecDusty <ecmgit@outlook.com>
2016-08-02 23:23:41 +0800

    Fix Gulp file issue, and small weather issue in edge

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 //Text Variables
 var strt = '>---Starting ',
-  end = ' Task---<';
+	end = ' Task---<';
 
 //Dependencies Needed
 var gulp = require('gulp');
@@ -37,26 +37,26 @@ var browserSync = require('browser-sync').create();
 
 //Source
 var SCRIPTS_PATH = 'src/js/{libs,**}/*.js',
-  HTML_PATH = 'src/{*.html,**/*.html}',
-  IMG_PATH = 'src/**/images/*.{png,jpeg,jpg,gif,svg}',
-  SCSS_PATH = 'src/styles/*.scss',
-  AUD_PATH = 'src/audio/*.mp3';
+	HTML_PATH = 'src/{*.html,**/*.html}',
+	IMG_PATH = 'src/**/images/*.{png,jpeg,jpg,gif,svg}',
+	SCSS_PATH = 'src/styles/*.scss',
+	AUD_PATH = 'src/audio/*.mp3';
 //	CSS_PATH = 'src/css/**/*.css', /*No Longer needed as styles are created in SASS*/ */
 
 
 //Distribution
 var DIST_DIR = 'dist',
-  DIST_CSS = 'dist/styles',
-  DIST_JS = 'dist/js',
-  DIST_IMG = 'dist/images',
-  DIST_AUD = 'dist/audio';
+	DIST_CSS = 'dist/styles',
+	DIST_JS = 'dist/js',
+	DIST_IMG = 'dist/images',
+	DIST_AUD = 'dist/audio';
 
 //Testing
 var TEST_DIR = 'test',
-  TEST_CSS = 'test/styles',
-  TEST_JS = 'test/js',
-  TEST_IMG = 'test/images/*.{png,jpeg,jpg,gif,svg}',
-  TEST_AUD = 'dist/audio';
+	TEST_CSS = 'test/styles',
+	TEST_JS = 'test/js',
+	TEST_IMG = 'test/images/*.{png,jpeg,jpg,gif,svg}',
+	TEST_AUD = 'dist/audio';
 
 
 
@@ -65,14 +65,14 @@ var TEST_DIR = 'test',
 =  for Dist  =
 =============*/
 gulp.task('sass-dist', function () {
-  console.log(strt + 'SASS Styles for DIST' + end);
+	console.log(strt + 'SASS Styles for DIST' + end);
 
-  return gulp.src(SCSS_PATH)
-  .pipe(sass({
-    outputStyle: 'compressed'
-    }).on('error', sass.logError))
-  .pipe(autoprefixer())
-  .pipe(gulp.dest(DIST_CSS));
+	return gulp.src(SCSS_PATH)
+	.pipe(sass({
+		outputStyle: 'compressed'
+		}).on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(gulp.dest(DIST_CSS));
 });
 
 
@@ -81,14 +81,14 @@ gulp.task('sass-dist', function () {
 =  for Dev   =
 =============*/
 gulp.task('sass-dev', function () {
-  console.log(strt + 'SASS Styles for DEV' + end);
+	console.log(strt + 'SASS Styles for DEV' + end);
 
-  return gulp.src(SCSS_PATH)
-  .pipe(srcMaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer())
-  .pipe(srcMaps.write())
-  .pipe(gulp.dest(TEST_CSS));
+	return gulp.src(SCSS_PATH)
+	.pipe(srcMaps.init())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(srcMaps.write())
+	.pipe(gulp.dest(TEST_CSS));
 });
 
 /*============
@@ -96,12 +96,12 @@ gulp.task('sass-dev', function () {
 =  for Dist  =
 =============*/
 gulp.task('scripts-dist', ['lint'], function () {
-  console.log(strt + 'SCRIPTS for DIST' + end);
+	console.log(strt + 'SCRIPTS for DIST' + end);
 
-  return gulp.src(SCRIPTS_PATH)
-  .pipe(concat('all.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest(DIST_JS));
+	return gulp.src(SCRIPTS_PATH)
+	.pipe(concat('all.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest(DIST_JS));
 });
 
 /*============
@@ -109,25 +109,25 @@ gulp.task('scripts-dist', ['lint'], function () {
 =   for Dev  =
 =============*/
 gulp.task('scripts-dev', ['lint'], function () {
-  console.log(strt + 'SCRIPTS DEV' + end);
+	console.log(strt + 'SCRIPTS DEV' + end);
 
-  return gulp.src(SCRIPTS_PATH)
-  .pipe(srcMaps.init())
-  .pipe(babel())
-  .pipe(concat('all.js'))
-  .pipe(srcMaps.write())
-  .pipe(gulp.dest(TEST_JS));
+	return gulp.src(SCRIPTS_PATH)
+	.pipe(srcMaps.init())
+	.pipe(babel())
+	.pipe(concat('all.js'))
+	.pipe(srcMaps.write())
+	.pipe(gulp.dest(TEST_JS));
 });
 
 /*============
 =    Lint    =
 =============*/
 gulp.task('lint', function () {
-  console.log(strt + 'Linting' + end);
-  return gulp.src(SCRIPTS_PATH)
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failOnError());
+	console.log(strt + 'Linting' + end);
+	return gulp.src(SCRIPTS_PATH)
+	.pipe(eslint())
+	.pipe(eslint.format())
+	.pipe(eslint.failOnError());
 })
 
 /*============
@@ -136,22 +136,22 @@ gulp.task('lint', function () {
 =============*/
 
 gulp.task('html-dist', function () {
-  console.log(strt + 'HTML for DIST' + end);
+	console.log(strt + 'HTML for DIST' + end);
 
-  return gulp.src(HTML_PATH)
-  .pipe(plumber(function (err) {
-    console.log('---HTML Task Error');
-    console.log(err);
-    console.log('----Error End');
-    this.emit('end');
-    }))
-  .pipe(htmlMinify({
-    collapseWhitespace: true,
-    minifyCSS: true,
-    minifyJS: true,
-    removeComments: true
-    }))
-  .pipe(gulp.dest(DIST_DIR));
+	return gulp.src(HTML_PATH)
+	.pipe(plumber(function (err) {
+		console.log('---HTML Task Error');
+		console.log(err);
+		console.log('----Error End');
+		this.emit('end');
+		}))
+	.pipe(htmlMinify({
+		collapseWhitespace: true,
+		minifyCSS: true,
+		minifyJS: true,
+		removeComments: true
+		}))
+	.pipe(gulp.dest(DIST_DIR));
 });
 
 /*============
@@ -160,22 +160,22 @@ gulp.task('html-dist', function () {
 =============*/
 
 gulp.task('html-dev', function () {
-  console.log(strt + 'HTML for DEV' + end);
+	console.log(strt + 'HTML for DEV' + end);
 
-  return gulp.src(HTML_PATH)
-  .pipe(plumber(function (err) {
-    console.log('---HTML Task Error');
-    console.log(err);
-    console.log('----Error End');
-    this.emit('end');
-    }))
-  .pipe(htmlMinify({
-    collapseWhitespace: true,
-    minifyCSS: false,
-    minifyJS: false,
-    removeComments: true
-    }))
-  .pipe(gulp.dest(TEST_DIR));
+	return gulp.src(HTML_PATH)
+	.pipe(plumber(function (err) {
+		console.log('---HTML Task Error');
+		console.log(err);
+		console.log('----Error End');
+		this.emit('end');
+		}))
+	.pipe(htmlMinify({
+		collapseWhitespace: true,
+		minifyCSS: false,
+		minifyJS: false,
+		removeComments: true
+		}))
+	.pipe(gulp.dest(TEST_DIR));
 });
 
 /*============
@@ -184,11 +184,11 @@ gulp.task('html-dev', function () {
 =============*/
 
 gulp.task('images-dist', function () {
-  console.log(strt + 'Images for DIST' + end);
+	console.log(strt + 'Images for DIST' + end);
 
-  return gulp.src(IMG_PATH)
-  .pipe(imgS())
-  .pipe(gulp.dest(DIST_DIR));
+	return gulp.src(IMG_PATH)
+	.pipe(imgS())
+	.pipe(gulp.dest(DIST_DIR));
 });
 
 /*============
@@ -196,10 +196,10 @@ gulp.task('images-dist', function () {
 =  for Dev   =
 =============*/
 gulp.task('images-dev', function () {
-  console.log(strt + 'Images for DEV' + end);
+	console.log(strt + 'Images for DEV' + end);
 
-  return gulp.src(IMG_PATH)
-  .pipe(gulp.dest(TEST_DIR));
+	return gulp.src(IMG_PATH)
+	.pipe(gulp.dest(TEST_DIR));
 });
 
 /*============
@@ -208,19 +208,19 @@ gulp.task('images-dev', function () {
 =============*/
 gulp.task('serve:dist', function() {
 
-  gulp.watch(SCRIPTS_PATH, ['scripts-dist']);
-  gulp.watch(HTML_PATH, ['html-dist']);
-  gulp.watch(SCSS_PATH, ['sass-dist']);
-  gulp.watch(IMG_PATH, ['images-dist']);
+	gulp.watch(SCRIPTS_PATH, ['scripts-dist']);
+	gulp.watch(HTML_PATH, ['html-dist']);
+	gulp.watch(SCSS_PATH, ['sass-dist']);
+	gulp.watch(IMG_PATH, ['images-dist']);
 
-  browserSync.init({
-  server: {
-    baseDir: './dist/',
-    domain: 'local.dev'
-  }
-  });
+	browserSync.init({
+	server: {
+		baseDir: './dist/',
+		domain: 'local.dev'
+	}
+	});
 
-  gulp.watch(['dist/{*.html,**/*.html}', 'dist/**/*.css', 'dist/**/*.js', 'dist/**/*.{png,jpeg,jpg,gif,svg}']).on('change', browserSync.reload);
+	gulp.watch(['dist/{*.html,**/*.html}', 'dist/**/*.css', 'dist/**/*.js', 'dist/**/*.{png,jpeg,jpg,gif,svg}']).on('change', browserSync.reload);
 });
 
 /*============
@@ -229,19 +229,19 @@ gulp.task('serve:dist', function() {
 =============*/
 gulp.task('serve:dev', function() {
 
-  gulp.watch(SCRIPTS_PATH, ['scripts-dev']);
-  gulp.watch(HTML_PATH, ['html-dev']);
-  gulp.watch(SCSS_PATH, ['sass-dev']);
-  gulp.watch(IMG_PATH, ['images-dev']);
+	gulp.watch(SCRIPTS_PATH, ['scripts-dev']);
+	gulp.watch(HTML_PATH, ['html-dev']);
+	gulp.watch(SCSS_PATH, ['sass-dev']);
+	gulp.watch(IMG_PATH, ['images-dev']);
 
-  browserSync.init({
-  server: {
-    baseDir: './test/',
-    domain: 'local.dev'
-  }
-  });
+	browserSync.init({
+	server: {
+		baseDir: './test/',
+		domain: 'local.dev'
+	}
+	});
 
-  gulp.watch(['test/{*.html,**/*.html}', 'test/**/*.css', 'test/**/*.js', 'test/**/*.{png,jpeg,jpg,gif,svg}']).on('change', browserSync.reload);
+	gulp.watch(['test/{*.html,**/*.html}', 'test/**/*.css', 'test/**/*.js', 'test/**/*.{png,jpeg,jpg,gif,svg}']).on('change', browserSync.reload);
 });
 
 /*============
@@ -249,10 +249,10 @@ gulp.task('serve:dev', function() {
 =    Task    =
 =============*/
 gulp.task('clean', function () {
-  return del.sync([
-  DIST_DIR,
-  TEST_DIR
-  ]);
+	return del.sync([
+	DIST_DIR,
+	TEST_DIR
+	]);
 });
 
 /*=====================
@@ -261,7 +261,7 @@ gulp.task('clean', function () {
 =         Task        =
 =====================*/
 gulp.task('dist', ['html-dist', 'sass-dist', 'scripts-dist', 'images-dist'], function () {
-  console.log('>---- Distribution  folder Created ----<');
+	console.log('>---- Distribution  folder Created ----<');
 });
 
 /*=====================
@@ -270,9 +270,9 @@ gulp.task('dist', ['html-dist', 'sass-dist', 'scripts-dist', 'images-dist'], fun
 =         Task        =
 =====================*/
 gulp.task('export', ['html-dist', 'sass-dist', 'scripts-dist', 'images-dist'], function () {
-  return gulp.src('{dist,src}/**')
-  .pipe(zip('website.zip'))
-  .pipe(gulp.dest('./'));
+	return gulp.src('{dist,src}/**')
+	.pipe(zip('website.zip'))
+	.pipe(gulp.dest('./'));
 });
 
 /*============
@@ -281,10 +281,10 @@ gulp.task('export', ['html-dist', 'sass-dist', 'scripts-dist', 'images-dist'], f
 =============*/
 //This function will clean out your distribution folder, and then update it with all the recent changes. After running this, it's best to run 'gulp serve' to get your live preview playing.
 gulp.task('default', [
-  'clean',
-  'html-dev',
-  'sass-dev',
-  'scripts-dev',
-  'images-dev',
-  'serve:dev'
+	'clean',
+	'html-dev',
+	'sass-dev',
+	'scripts-dev',
+	'images-dev',
+	'serve:dev'
 ]);
